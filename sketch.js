02fa5bb38974fb7e336c21eb9bf09e2c8ad014e2
Ad115 <a.garcia230395@gmail.com>
2018-01-08 10:12:48 -0600

    Load chromosome data to an object.

--- sketch.js
+++ sketch.js
@@ -16,12 +16,16 @@ var centromereStart = 122026460;
 var centromereEnd = 125184587;
 
 var table;
+var data;
 
 function preload() {
   table = loadTable('data/centromeric-regions.tsv', 'tsv', 'header');
 }
 
 function setup() {
+    
+    data = loadChromosomesData(table);
+  print(data);
   createCanvas(windowWidth, windowHeight);
   background(220);
 
@@ -30,17 +34,6 @@ function setup() {
   print(table.getColumnCount() + ' total columns in table');
 
   print(table.columns);
-  let species = table.getColumn("Species").filter((v, i, a) => a.indexOf(v) === i);
-  print(species);
-
-  //cycle through the table
-  for (var r = 0; r < table.getRowCount(); r++) {
-    var row = ''
-    for (var c = 0; c < table.getColumnCount(); c++) {
-      row += table.getString(r, c) + "\t";
-    }
-    print(row);
-  }
 }
 
 function draw() {
@@ -48,7 +41,7 @@ function draw() {
   var left = centromereStart;
   var right = chromosomeLength - centromereEnd;
   var maxSide = Math.max(left, right);
-  var margin = 300;
+  var margin = 30;
   var scale = (width/2-margin) / maxSide;
 
   drawChromosome(scale*left, scale*right);
@@ -74,4 +67,62 @@ function drawChromosome(left, right) {
   ellipse(0, 0, centromereWidth, centromereWidth);
 
 
+}
+
+function loadChromosomesData(table) { /*
+    Load the data in the given table into an object 
+    with the following schema:
+    
+         SPECIES        CHROM    CHROMOSOME DATA
+         -------        -----    ----------------
+    
+    { 'Homo sapiens' : { '1' : { 'length': XXXX,
+                                 'centromere': XXXX }
+                         '2' : { ... },
+                        
+                         ...
+                        
+                         'Y' : { ... } },
+                        
+      'Mus musculus' : { ... },
+      ...
+      'Gallus gallus': { ... } }
+    
+    */
+    
+     for (var r = 0; r < table.getRowCount(); r++) {
+    var row = ''
+    for (var c = 0; c < table.getColumnCount(); c++) {
+      row += table.getString(r, c) + "\t";
+    }
+    print(row);
+  }
+    
+    var data = {};
+    for (var r = 0; r < table.getRowCount(); r++) {
+        
+        var row = table.getRow(r);
+        
+        // Fetch species data
+        var species = row.getString('species');
+        if (!data[species])
+            data[species] = {}; // Create species object if not exists already
+        print(species);
+        
+        // Fetch chromosome data
+        var chrom = row.getString('chromosome');
+        if (!data[species][chrom])
+            data[species][chrom] = {}; // Create chromosome object if not exists already
+        
+        // Chromosome length
+        var chrom_length = Number(row.getString('chromosome length (bp)'));
+        data[species][chrom]['length'] = chrom_length;
+        
+        // Centromere position
+        var chrom_cent = (  Number(row.getString('centromeric region start')) 
+                          + Number(row.getString('centromeric region end'))   )/2; // Just in the middle.
+        data[species][chrom]['centromere'] = chrom_cent;
+    }
+    
+    return data;
 }
