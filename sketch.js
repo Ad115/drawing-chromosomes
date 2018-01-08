ae352eb5670985d03315ada9ee1125431f165a14
Ad115 <a.garcia230395@gmail.com>
2018-01-08 12:45:18 -0600

    Finally, version 1.0!

--- sketch.js
+++ sketch.js
@@ -18,111 +18,155 @@ var centromereEnd = 125184587;
 var table;
 var data;
 
-function preload() {
+function preload() { /*
+    File I/O is asynchronous, so it must be in the
+    preload function to ensure data has been read
+    before it is used.
+    */
   table = loadTable('data/centromeric-regions.tsv', 'tsv', 'header');
 }
 
-function setup() {
+function setup() {/*
+    Initial environment setup.
+    */
     
     data = loadChromosomesData(table);
-  print(data);
-  createCanvas(windowWidth, windowHeight);
-  background(220);
-
-  //count the columns
-  print(table.getRowCount() + ' total rows in table');
-  print(table.getColumnCount() + ' total columns in table');
-
-  print(table.columns);
+    print(data)
+    
+    // Create and prepare drawing canvas.
+    createCanvas(windowWidth, 5000);
+    
+    background(220);
+    textAlign(CENTER, BOTTOM);
+    textSize(32);
+    
+    var y = 0;
+    for (var species in data){
+        // --- Draw the species name
+        y += 50;
+        text(species, width/2, y);
+        
+        // --- Draw the chromosomes
+        y += 10;
+        
+        // Prepare to draw
+        var maxSide = data[species]
+                        .map(getLongestSide)
+                        .reduce( (a, b) => Math.max(a, b) );
+        var margin = 30;
+        var scale = (width/2-margin) / maxSide;
+        
+        // Draw the chromosomes and the chromosome name
+        for(var i=0; i < data[species].length; i++) {
+            chromosome = data[species][i];
+            
+            text(chromosome.name, 20, y+20);
+            drawChromosome(chromosome, width/2, y, scale);
+            y += 30
+        }
+        
+        
+    }
 }
 
-function draw() {
-  // Calculate scaling
-  var left = centromereStart;
-  var right = chromosomeLength - centromereEnd;
-  var maxSide = Math.max(left, right);
-  var margin = 30;
-  var scale = (width/2-margin) / maxSide;
-
-  drawChromosome(scale*left, scale*right);
+function draw() { /*
+    Main drawing loop.
+    */
 }
 
 
-function drawChromosome(left, right) {
-/* Draw a chromosome.*/
-  var centromereWidth = 20;
-  var chromosomeWidth = 17;
+function drawChromosome(chromosome, x, y, scale) { /* 
+    Draw a chromosome centered on the centromere 
+    at coordinates x,y scaled by a factor.
+    */
+    
+    if (!scale)
+      scale = 1; // Scale is optional argument
+    let length = scale * chromosome.length
+    let centromere = scale * (chromosome.centromere 
+                                ? chromosome.centromere
+                                : length/2);
+    // Constants
+    let centromereRadius = 18;
+    let chromosomeWidth = 17;
 
-  // Begin drawing
-  translate(width/2, height/2); // Coordinates relative to the center of the screen.
+    push(); // Prepare for a coordinates change
+        translate(x, y); // Coordinates relative to the point x,y
 
-  // Draw chromosome sides
-  stroke(150);
-  strokeWeight(chromosomeWidth);
-  line(-left, 0, right, 0);
+        // Draw chromosome sides
+        stroke(150);
+        strokeWeight(chromosomeWidth);
+        line(-centromere, 0, length-centromere, 0);
 
-  // Draw centromere
-  noStroke();
-  fill(100);
-  ellipse(0, 0, centromereWidth, centromereWidth);
-
-
+        // Draw centromere
+        if (chromosome.centromere) {
+            noStroke();
+            fill(100);
+            ellipse(0, 0, centromereRadius, centromereRadius);
+        }
+    pop(); // Restore previous coordinates
 }
 
 function loadChromosomesData(table) { /*
     Load the data in the given table into an object 
     with the following schema:
     
-         SPECIES        CHROM    CHROMOSOME DATA
-         -------        -----    ----------------
+         SPECIES        CHROMOSOME DATA
+         -------        ---------------
     
-    { 'Homo sapiens' : { '1' : { 'length': XXXX,
-                                 'centromere': XXXX }
-                         '2' : { ... },
+    { 'Homo sapiens' : [ { 'name': 'I',
+                           'length': XXXX,
+                           'centromere': XXXX },
+                         { ... },
+                         { ... },
                         
-                         ...
+                         ... 
+                         { 'name': 'XIV',
+                           'length': XXXX,
+                           'centromere': XXXX },],
                         
-                         'Y' : { ... } },
-                        
-      'Mus musculus' : { ... },
+      'Mus musculus' : [ ... ],
       ...
-      'Gallus gallus': { ... } }
+      'Gallus gallus': [ ... ] }
     
     */
-    
-     for (var r = 0; r < table.getRowCount(); r++) {
-    var row = ''
-    for (var c = 0; c < table.getColumnCount(); c++) {
-      row += table.getString(r, c) + "\t";
-    }
-    print(row);
-  }
-    
     var data = {};
     for (var r = 0; r < table.getRowCount(); r++) {
         
-        var row = table.getRow(r);
+        let row = table.getRow(r);
         
-        // Fetch species data
-        var species = row.getString('species');
+        // --- Fetch species data
+        let species = row.getString('species');
         if (!data[species])
-            data[species] = {}; // Create species object if not exists already
-        print(species);
+            data[species] = []; // Create species list if doesn't already exists
         
-        // Fetch chromosome data
-        var chrom = row.getString('chromosome');
-        if (!data[species][chrom])
-            data[species][chrom] = {}; // Create chromosome object if not exists already
+        // --- Fetch chromosome data
+        chromosome = {};
+        
+        // Chromosome name
+        chromosome.name = row.getString('chromosome');
         
         // Chromosome length
-        var chrom_length = Number(row.getString('chromosome length (bp)'));
-        data[species][chrom]['length'] = chrom_length;
+        chromosome.length = Number(row.getString('chromosome length (bp)'));
         
         // Centromere position
-        var chrom_cent = (  Number(row.getString('centromeric region start')) 
-                          + Number(row.getString('centromeric region end'))   )/2; // Just in the middle.
-        data[species][chrom]['centromere'] = chrom_cent;
+        let cenStart = Number(row.getString('centromeric region start'));
+        let cenEnd = Number(row.getString('centromeric region end'))
+        chromosome.centromere = ( cenStart + cenEnd )/2; // Just in the middle.
+        
+        data[species].push(chromosome);
     }
     
     return data;
+}
+
+function getLongestSide(chromosome) { /*
+    From a chromosome object:
+    c = { 'name': 'IV',
+          'length': XXXX,
+          'centromere': XXXX },
+    get the longest of `c.centromere` and `c.length - c.centromere`
+    */
+    let centromere = chromosome.centromere
+    return Math.max(centromere, chromosome.length-centromere);
 }
